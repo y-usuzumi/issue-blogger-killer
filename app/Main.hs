94c4ea95c6c6090575ae3c46657cffc285d837de
Yukio Usuzumi <anohigisavay@gmail.com>
2018-07-13 02:43:59 +0800

    Done

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,35 @@
 module Main where
 
-import Lib
+import           Control.Concurrent
+import           Control.Monad
+import           Data.IORef
+import           IBK.Context
+import           IBK.Req
+import           System.IO.Unsafe
+import           System.Random
+import           Test.RandomStrings
+import           Text.Printf
+
+isoAlpha = onlyAlpha randomChar8
+asciiAlphanum = onlyAlphaNum randomASCII
+
+{-# NOINLINE context #-}
+context :: IORef Context
+context = unsafePerformIO $ newIORef Context{ username = ""
+                                            , password = ""
+                                            , targetUser = ""
+                                            , targetRepo = ""
+                                            }
 
 main :: IO ()
-main = someFunc
+main = do
+  ctx <- readIORef context
+  replicateM_ 480 $ do
+    putStrLn "Creating issue"
+    (title:body:_) <- randomStringsLen (randomString (onlyPrintable randomChar)) (5,25) 2
+    let createIssue = CreateIssue{ title = title
+                                 , body = body
+                                 }
+    void $ req createIssue ctx
+    (p :: Int) <- randomRIO (5000000, 40000000)
+    printf "Sleeping for %f secs" (fromIntegral p/1000000 :: Double)
